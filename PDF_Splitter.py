79b86b0ff8e5b678d80d81b797cf4148fb1adf6a
matthausen <32019118+matthausen@users.noreply.github.com>
2018-08-18 17:31:26 +0100

    Update PDF_Splitter.py

--- PDF_Splitter.py
+++ PDF_Splitter.py
@@ -3,16 +3,7 @@ import glob, os
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from os.path import expanduser
 
-
 class Split:
-	#Show some instructions
-	print (" ")
-	print (" ")
-	print (" ")
-	print (" ")
-	print (" ")
-	print (" ")
-	print (" ")
 	print ("###############PDF_SPLITTER############")
 	print ("Instructions: ")
 	print ("1)Create a folder caled 'PDF' on your Desktop")
@@ -23,45 +14,37 @@ class Split:
 	print ("#######################################")
 	print ("#######################################")
 
+def split(): 
+        #Find path with file
+	from os.path import expanduser
+environment = expanduser("~/Desktop/PDF")
+os.chdir(environment)
+mypdf = "MyPdf.pdf"
+pdf_file = open(os.path.join(os.getcwd(), "MyPdf.pdf"),"rb")
+pdf_reader = PdfFileReader(pdf_file)
+pdf_writer = PdfFileWriter()
 
-	def search_destroy():
+    #Prompt user which page to pslit
+split = input("Which page you want to keep?  ")
+convert = int(split)
+pdf_writer.addPage(pdf_reader.getPage(convert -1))
+question = input ("Any more pages?......type y/n:  ")
 
-		#Find path with file
-		from os.path import expanduser
-        environment = expanduser("~/Desktop/PDF")
-        os.chdir(environment)
-
-    	pdf_file = open ("C:\Users\Matthausen\Desktop\PDF\MyPdf.pdf", "rb")
-	pdf_reader = PdfFileReader(pdf_file)
-	pdf_writer = PdfFileWriter()
-
-        #Prompt user which page to pslit
-	split = raw_input("Which page you want to keep?  ")
-	convert = int(split)
-	pdf_writer.addPage(pdf_reader.getPage(convert -1))
-
-	question = raw_input ("Any more pages?......type y/n:  ")
-
-	while (question == "y"):
-		newsplit = raw_input("Which page you want to keep?  ")
-		newconvert = int(newsplit)
-		pdf_writer.addPage(pdf_reader.getPage(newconvert -1))
-		question = raw_input ("Any more pages?......type y/n:  ")
+while (question == "y"):
+	newsplit = input("Which page you want to keep?  ")
+	newconvert = int(newsplit)
+	pdf_writer.addPage(pdf_reader.getPage(newconvert -1))
+	question = input ("Any more pages?......type y/n:  ")
 
 
 
-	split_file = open("C:\Users\Matthausen\Desktop\PDF\MySplit.pdf", "wb")
-	pdf_writer.write(split_file)
-	split_file.close()
-	pdf_file.close()
+split_file = open(os.path.join(os.getcwd(), "NewSplit.pdf"), "wb")
+pdf_writer.write(split_file)
+split_file.close()
+pdf_file.close()
 
+print ("Your new file is called 'NewSplit'")
+print ("#######################################")
+print ("Thank you for using me! Bye Bye...")
 
-	print (" ")
-	print (" ")
-	print (" ")
-	print ("Your new file is called 'MySplit'")
-	print ("#######################################")
-	print ("Thank you for using me! Bye Bye...")
-
-
-
+split()
